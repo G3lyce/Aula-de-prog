a0af65570faa84fecaa48a10bdda2b6befdeb266
G3lyce <102549213+G3lyce@users.noreply.github.com>
2022-05-30 15:45:42 -0300

    Add files via upload

---
+++ numero maior.py
@@ -0,0 +1,5 @@
+a=int(input())
+if (a<=36):
+    print('correto')
+else:
+    print('errado')
---
+++ senha e usuario.py
@@ -0,0 +1,12 @@
+Usuário=str(input("Insira seu nome de usuário "))
+Senha=str(input("Insira sua senha "))
+
+if (Usuário=="Gelyce"):
+    print("Usuário correto")
+else:
+    print("Usuário errado")
+
+if (Senha=="12345"):
+    print("Senha correta")
+else:
+    print("Senha incorreta")
